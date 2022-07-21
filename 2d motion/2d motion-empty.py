631e80fae1c1a4c76df243b2ae02e0fa964b8cd6
UnKnownnPasta <109729125+UnKnownnPasta@users.noreply.github.com>
2022-07-21 18:52:57 +0530

    Coming soon

--- 2d motion/2d motion-empty.py
+++ 2d motion/2d motion-empty.py
@@ -4,24 +4,21 @@ from random import randrange
 # -----------INITIALIZATION
 print('\nMove:', ''.join(baseLineMod), end='\n')
 STORE_DATA, i, n, l = [], 0, 30, 0
-# more soon
 
 # -----------TERRAIN
 """
 to be added.
 
 adding:
-    better layout of squares
-    bigger area to move in
+    better layout of squares -- done
+    bigger area to move in -- done
 """
-
 
 # -----------MOVEMENT
 """
 to be added.
 
 adding:
-    w, a, s, d in x and y axis
-    soon; move two squares at once (left/right only)
+    w, a, s, d in x and y axis -- done
+    soon; move two squares at once (left/right only) -- not yet
 """
-
