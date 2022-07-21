6c6951088d980975fcc6c97715d0d9fb4e46b654
UnKnownnPasta <109729125+UnKnownnPasta@users.noreply.github.com>
2022-07-21 14:54:41 +0530

    Update 2d motion-empty.py

--- 2d motion/2d motion-empty.py
+++ 2d motion/2d motion-empty.py
@@ -4,26 +4,24 @@ from random import randrange
 # -----------INITIALIZATION
 print('\nMove:', ''.join(baseLineMod), end='\n')
 STORE_DATA, i, n, l = [], 0, 30, 0
-
+# more soon
 
 # -----------TERRAIN
-changes = ['🟫','🟦','🟩','🟩','🟦','🟩']
+"""
+to be added.
 
-while i <= 10:
-    for x in '1234567890abcdefghijploasdcxzv':
-        randCbLock = randrange(-1,5)
-        baseLineMod[n-1] = changes[randCbLock]
-        if n <= 0:
-            n = 30
-            i+=1
-        else:
-            n -= 1
-    dupeList = tuple(baseLineMod)
-    STORE_DATA.append(''.join(baseLineMod))
-    print(''.join(dupeList), end='\n')
+adding:
+    better layout of squares
+    bigger area to move in
+"""
 
 
 # -----------MOVEMENT
-while l < 1: 
-    c = input()
+"""
+to be added.
+
+adding:
+    w, a, s, d in x and y axis
+    soon; move two squares at once (left/right only)
+"""
 
